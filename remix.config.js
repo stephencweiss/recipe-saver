e564134f46f3bd1c924620d4428fc4d00f6f875d
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-11-08 21:34:24 -0600

    Future proofing a bit
    Not really sure what this is goin to do, but since I am just getting
    started, seems safe to opt into v3 since we know it's coming.

--- remix.config.js
+++ remix.config.js
@@ -3,4 +3,7 @@ module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.test.{ts,tsx}"],
   serverModuleFormat: "cjs",
+  future: {
+    v3_fetcherPersist: true,
+  }
 };
