682895f2b8de2e8ffa3c82c3582ed93082b58dba
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-12-28 15:50:45 -0600

    Remove coupling of recipes and comments

--- app/api/recipe-loader.ts
+++ app/api/recipe-loader.ts
@@ -1,15 +1,12 @@
-import { json } from "@remix-run/node";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
-import { RecipeUserArgs, getPrivateRecipeComments, getPublicRecipeComments, getRecipeWithIngredients } from "~/models/recipe.server";
+import { RecipeUserArgs, getRecipeWithIngredients } from "~/models/recipe.server";
 import { getUser } from "~/session.server";
 import { isNotPlaceholderIngredient, parsePreparationSteps } from "~/utils";
 
-// TODO: Load comments separately (as part of the api.comments.tsx file)
-// Then remove the comments from this loader
 /** A common loader for a specific recipe; include the requesting user */
-export async function loadSingleRecipe({ params, request, mode }: LoaderFunctionArgs & { mode: 'edit' | 'view' }) {
+export async function loadSingleRecipe({ params, request, mode }: Pick<LoaderFunctionArgs, "params" | "request"> & { mode: 'edit' | 'view';}) {
   invariant(params.recipeId, "recipeId not found");
   const user = await getUser(request);
 
@@ -19,8 +16,6 @@ export async function loadSingleRecipe({ params, request, mode }: LoaderFunction
   }
 
   const rawRecipe = await getRecipeWithIngredients(args);
-  const privateComments = await getPrivateRecipeComments(args);
-  const publicComments = await getPublicRecipeComments(args);
   if (!rawRecipe) {
     throw new Response("Not Found", { status: 404 });
   }
@@ -28,14 +23,13 @@ export async function loadSingleRecipe({ params, request, mode }: LoaderFunction
     throw new Response("Not Found", { status: 404 });
   }
   if (mode == 'edit' && rawRecipe.submittedBy !== user?.id) {
-    throw new Response("Cannot Edit Recipe Owned by Other User", { status: 401 });
+    throw new Response("Cannot edit a recipe owned by another user", { status: 401 });
   }
 
   const recipe = {
     ...rawRecipe,
     recipeIngredients: rawRecipe?.recipeIngredients.map(r => ({ ...r, isDeleted: false })).filter(isNotPlaceholderIngredient) ?? [],
     preparationSteps: parsePreparationSteps(rawRecipe.preparationSteps ?? ""),
-    comments: [...privateComments, ...publicComments],
   };
-  return json({ recipe, user })
+    return { recipe, user };
 }
