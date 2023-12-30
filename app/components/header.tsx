7f78841261a203a70b205e41dcf2b8c4feac2cc5
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-12-29 18:36:46 -0600

    update header
    instead of having a hamburger menu, i now put the menu under the "title"
    of the page.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,11 @@
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { LinksFunction } from "@remix-run/node";
 import { Form, Link } from "@remix-run/react";
 import { HTMLProps } from "react";
 
 import { useOptionalUser } from "~/utils";
 
-import { Menu, links as menuLinks } from "./menu";
+import { HeaderNavMenu, links as menuLinks } from "./menu";
 
 export const links: LinksFunction = () => [...menuLinks()];
 
@@ -18,7 +19,7 @@ const BasicHeader = ({
   title,
   children,
 }: React.PropsWithChildren<Readonly<HeaderProps>>) => {
-  console.log("className", className)
+  console.log("className", className);
   return (
     <header
       className={
@@ -29,24 +30,21 @@ const BasicHeader = ({
         className
       }
     >
-      <h1 className="text-3xl font-bold">
-        <Link to="/explore">{title}</Link>
-      </h1>
-
+      <HeaderNavMenu>
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <HamburgerMenuIcon />
+      </HeaderNavMenu>
       {children}
     </header>
   );
 };
 
-const StandardHeader = ({
-  title,
-  className,
-}: Readonly<HeaderProps>) => {
+const StandardHeader = ({ title, className }: Readonly<HeaderProps>) => {
   const user = useOptionalUser();
   const displayName = user?.name || user?.email;
   return (
     <BasicHeader title={title} className={className}>
-      <div className="items-center">
+      <div className="items-center hidden md:flex">
         {displayName ? (
           <Link to={`/user/${user.id}/profile`} className="text-xl">
             {displayName}
@@ -55,11 +53,13 @@ const StandardHeader = ({
           <></>
         )}
       </div>
+
       <div className="flex flex-row gap-4">
-        {user ? (
-          <Link
-            to="/recipes/new?submissionStyle=create-manual"
-            className="
+        <div className="hidden md:flex">
+          {user ? (
+            <Link
+              to="/recipes/new?submissionStyle=create-manual"
+              className="
       px-4 py-2 rounded
       bg-blue-500 text-white text-xl
       hover:bg-blue-600
@@ -67,18 +67,18 @@ const StandardHeader = ({
       active:bg-blue-700
       disabled:bg-gray-400
       "
-          >
-            + New Recipe
-          </Link>
-        ) : (
-          <Link
-            to="/explore"
-            className="rounded bg-slate-600 px-4 py-2 text-blue-100 text-xl hover:bg-blue-500 active:bg-blue-600"
-          >
-            Explore
-          </Link>
-        )}
-
+            >
+              + New Recipe
+            </Link>
+          ) : (
+            <Link
+              to="/explore"
+              className="rounded bg-slate-600 px-4 py-2 text-blue-100 text-xl hover:bg-blue-500 active:bg-blue-600"
+            >
+              Explore
+            </Link>
+          )}
+        </div>
         {user ? (
           <Form action="/logout" method="post">
             <button
@@ -91,22 +91,16 @@ const StandardHeader = ({
         ) : (
           <></>
         )}
-        <Menu />
       </div>
     </BasicHeader>
   );
 };
 
 export function Header({ title }: Readonly<HeaderProps>): JSX.Element {
-
   return (
     <>
-      <StandardHeader title={title} className={"hidden md:flex"} />
-      <BasicHeader title={title} className="md:hidden">
-        <div className="w-full flex flex-1 justify-end">
-          <Menu />
-        </div>
-      </BasicHeader>
+      <StandardHeader title={title} />
+      {/* <BasicHeader title={title} className="md:hidden" /> */}
     </>
   );
 }
