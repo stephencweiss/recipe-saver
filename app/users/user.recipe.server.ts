7a7d01a5d1b89ba697ec474cb0a830634e629808
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-12-30 00:52:50 -0600

    linting

--- app/users/user.recipe.server.ts
+++ app/users/user.recipe.server.ts
@@ -40,8 +40,6 @@ export async function markRecipeAsCooked(recipeId: Recipe["id"], userId: User["i
       cookCount: 1,
     },
   });
-
-
 }
 
 export async function getCookCounts(recipeId: Recipe["id"], userId?: User["id"]) {
