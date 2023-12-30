493cbe40c38a1dc773a123525f2a883987849f83
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-12-29 22:14:02 -0600

    make lists collapsible
    open by default

--- app/components/lists.tsx
+++ app/components/lists.tsx
@@ -9,8 +9,26 @@ const List = (props: ListProps) => {
   const { title, items, HeaderLevel = "h2", ListType = "ul" } = props;
 
   return (
-    <>
-      <HeaderLevel className="text-xl font-bold py-4">{title}</HeaderLevel>
+    <details open className="[&_svg]:open:-rotate-180 my-2">
+      <summary className="flex justify-between cursor-pointer list-none items-center gap-4">
+        <div className="flex gap-2 items-center">
+          {/* <!-- notice here, we added our own triangle/arrow svg --> */}
+          <svg
+            className="rotate-0 transform text-blue-700 transition-all duration-300"
+            fill="none"
+            height="20"
+            width="20"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+          >
+            <polyline points="6 9 12 15 18 9"></polyline>
+          </svg>
+          <HeaderLevel className="text-xl font-bold py-4">{title}</HeaderLevel>
+        </div>
+      </summary>
       {items.length === 0 ? (
         <p className="pb-2">Nothing to see here!</p>
       ) : (
@@ -27,7 +45,7 @@ const List = (props: ListProps) => {
           })}
         </ListType>
       )}
-    </>
+    </details>
   );
 };
 
